671612ad235f441b1afbd5dfae9bf8ee2eee41ce
Shereen Elsayed <shereenhussein92@gmail.com>
2022-04-19 23:00:41 -0700

    fixes

--- HW_1/Problem4.py
+++ HW_1/Problem4.py
@@ -15,6 +15,17 @@ def sum_elements(a):
         sum += a[i:i+8]
     return sum
 
+def getcol (matrix, col):
+    column = rtl.WireVector(bitwidth=2*8)
+    n = 8
+    for i in range(col*n, len(matrix), 2*n):
+        column += matrix[i:i+n]
+    return column
+
+def getrow(matrix, row):
+    print(type(matrix))
+    return matrix[row*2*8 : row*2*8+(2*8)] 
+
 def multiply(a, b):
     a_counter = rtl.Register(bitwidth=3, name='a_counter')
     b_counter = rtl.Register(bitwidth=3, name='b_counter')
@@ -23,26 +34,34 @@ def multiply(a, b):
 
     #Assumed dimensions => 2X2
     with rtl.conditional_assignment:
-        with a_counter == 0 & b_counter == 0: #initial
-                temp = elemwise_product(a[:(2*8)], b[:(2*8)])
+        with a_counter == 0 :
+            with b_counter == 0: #initial
+                temp = elemwise_product(getrow(a, a_counter),getcol(b, b_counter))
                 sum = sum_elements(temp)
-                c[a_counter][b_counter] = temp
-                a_counter.next |= a_counter+1
+                c[-1: -(2*8)] = sum # because elem 0 is at the right hand side
+                b_counter.next |= b_counter+1 #Increment b so we will be able to add rows first to the result
         
         with a_counter == 2 & b_counter == 2: # Last step
-            temp = sum(a[a_counter] * b[b_counter])
-            c[a_counter][b_counter] = temp
+            temp = elemwise_product(getrow(a, a_counter),getcol(b, b_counter))
+            sum = sum_elements(temp)
+            c[0:2*8] = sum
 
-        with a_counter == 2:
-            temp = sum(a[a_counter] * b[b_counter])
-            c[a_counter][b_counter] = temp
-            a_counter.next |= 0
-            b_counter.next |= b_counter+1
+        with b_counter == 2:
+            temp = elemwise_product(getrow(a, a_counter),getcol(b, b_counter))
+            sum = sum_elements(temp)
+            c[((a_counter*2) + b_counter)*8: (((a_counter*2) + b_counter)*8) + (2*8)] = sum
+            a_counter.next |= a_counter + 1
+            b_counter.next |= 0
         
         with rtl.otherwise:
-            temp = sum(a[a_counter] * b[b_counter])
-            c[a_counter][b_counter] = temp
-            a_counter.next |= a_counter+1
+            temp = elemwise_product(getrow(a, a_counter),getcol(b, b_counter))
+            sum = sum_elements(temp)
+            c[((a_counter*2) + b_counter)*8: (((a_counter*2) + b_counter)*8) + (2*8)] = sum
+            
+            b_counter.next |= b_counter+1
+
+
+matrix_c <<= multiply(matrix_a, matrix_b)
 
 a = [[1, 2], [3, 4]]
 b = [[1, 0], [1, 0]]
@@ -57,7 +76,7 @@ sim_inputs = {
 }
 sim_trace = rtl.SimulationTrace()
 sim = rtl.Simulation(tracer=sim_trace)
-
+sim.step_multiple(sim_inputs)
 
 for cycle in range(20):
     sim.step({})
