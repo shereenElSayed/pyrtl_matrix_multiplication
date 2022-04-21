b356ddd490d0519168bcfdcd78ea9db77f64d3eb
drholmie <animesh.leo@gmail.com>
2022-04-20 20:25:50 -0700

    Fixed bugs, need to fix warp simulation

--- HW_1/anime_p4.py
+++ HW_1/anime_p4.py
@@ -1,40 +1,49 @@
 import pyrtl
 #multiply 16 bit numbers in matrixes uptil 16*16 values
 
-#global memory
-shared_mem = pyrtl.MemBlock(32, 8, "shared_mem", 1, 10)
-#global counter
-counter = pyrtl.Register(8, "counter")
-#input buffers
-row_buffer = pyrtl.MemBlock(16, 4, "row_mem", 10, 0)
-col_buffer = pyrtl.MemBlock(16, 4, "col_mem", 10, 0)
 
-def kachonk(a, b, prev_sum):
-    mul = pyrtl.WireVector(32,"mul")
-    agg = pyrtl.WireVector(32,"agg")
+def kachonk(a, b, prev_sum, i):
+    mul = pyrtl.WireVector(32,"mul"+str(i))
+    agg = pyrtl.WireVector(32,"agg"+str(i))
     mul <<= a*b
-    #to aggregate or to sum
-    with pyrtl.conditional_assignment:
-        with prev_sum != 0: #agg value exists
-            agg |= prev_sum + a + b #aggregate new values
-        with prev_sum == 0: #agg value doesn't exist
-            agg |= a+b #aggregate current values
+    agg <<= a+b+prev_sum
     return agg
 
-def kachonker(row_a, col_b, w_mem):
+def kachonker(row_a, col_b):
     #both row_a and col_b would be 10 (16 bit) registers. 
-    prev_sum = pyrtl.WireVector(16,"prev_sum")
+    prev_sum = pyrtl.Const(0, 32)
+    # agg = pyrtl.WireVector(32,"agg")
+    #global memory
+    shared_mem = pyrtl.MemBlock(32, 8, "shared_mem", 1, 10)
+    #global counter
+    counter = pyrtl.Input(8, "counter")
     for i in range(10):
         #warp design, following 10 kachonks
-        counter.next <<= counter + 1
+        agg = kachonk(row_a[i][1:], col_b[i][1:], prev_sum, i)
         with pyrtl.conditional_assignment:
-            with w_mem == 1:
-                shared_mem[counter] |= kachonk(row_a[i], col_b[i], prev_sum)
-                prev_sum |= pyrtl.Const(0, 32)
+            with row_a[0] == 1:
+                shared_mem[counter] |= agg
+                prev_sum = pyrtl.Const(0, 32)
             with pyrtl.otherwise:
-                prev_sum |= kachonk(row_a[i], col_b[i], prev_sum)
-    
+                prev_sum = agg
+    return prev_sum
 
+#input buffers
+#first bit tells us whether to write into memory or not
+row_buffer = pyrtl.MemBlock(17, 4, "row_buffer", 20, 0)
+col_buffer = pyrtl.MemBlock(17, 4, "col_buffer", 20, 0)
+row_a = pyrtl.WireVector()
+dummy = kachonker(row_buffer, col_buffer)
+
+sim_trace = pyrtl.SimulationTrace()
+sim = pyrtl.Simulation(tracer=sim_trace)
+
+
+for cycle in range(20):
+    sim_vals = {"row_buffer": [1,0], "col_buffer":[0,1],"counter":cycle}
+    sim.step()
+   
+sim_trace.render_trace()
 
 
 
